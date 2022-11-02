d80efb80bc4c746b9f14093ed20f111319f728d3
lwetheng <lwetheng@iastate.edu>
2022-11-01 22:43:35 -0500

    Updated code

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-// var rs = require('readline-sync');
-import express from 'express';
-import { collectDefaultMetrics, register } from 'prom-client';
+var express = require('express')
+var prom = require('prom-client')
 
-collectDefaultMetrics();
+prom.collectDefaultMetrics();
 
 const app = express();
 
 app.get('/metrics', async (_req, res) => {
   try {
-    res.set('Content-Type', register.contentType);
-    res.end(await register.metrics());
+    res.set('Content-Type', prom.register.contentType);
+    res.end(await prom.register.metrics());
   } catch (err) {
     res.status(500).end(err);
   }
@@ -19,26 +18,50 @@ app.listen(4001, '0.0.0.0');
 
 console.log("GRAFANA NODE PROGRAM!");
 
-// var num1 = parseInt(rs.question('1st Number: '));
-// var num2 = parseInt(rs.question('2nd Number: '));
-// var action = rs.question('Enter the action [+, -, *, /]');
 
-// switch(action){
-//     case '+':
-//         console.log(num1 + num2)
-//         break;
-//     case '-':
-//         console.log(num1 - num2)
-//         break;
-//     case '*':
-//         console.log(num1 * num2)
-//         break;
-//     case '/':
-//         console.log(num1 / num2)
-//         break;
-//     default:
-//         console.log("Did not understand your input")
-//         break;
-// }
+const client = require('prom-client');
+let register = new client.Registry();
+
+const trafficCounter = new client.Counter({
+  name: 'traffic_counter',
+  help: 'Number of time route is called',
+});
+
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['route'],
+  // buckets for response time from 0.1ms to 500ms
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]
+})
+
+register.registerMetric(trafficCounter)
+
+register.setDefaultLabels({
+    app: 'test-api'
+})
+
+client.collectDefaultMetrics({ register });
+
+app.get('/metrics', async (request, response) => {
+    response.setHeader('Content-type', register.contentType);
+    response.end(await register.metrics());
+})
+
+app.get('/traffic-test', (request, response) => { //path to count calls in /traffic-test route
+    console.log("CALLING traffic-test")
+    trafficCounter.inc() //increment traffic counter
+    var currentTime = Date.now();
+
+    httpRequestDurationMicroseconds //observe response time in ms
+        .labels(request.route.path)
+        .observe(currentTime)
+
+    response.json({
+        message: "You called traffic-test",
+    });
+
+})
+
 
 
